a38ff0ba53a81be0d8ea6ae8a4363dd1125f4ec4
Rafal <rafal.opial@coderslab.pl>
2020-12-12 14:20:23 +0100

    m2d1t2z1 toString Example.

--- src/main/java/pl/coderslab/oop/firstclass/Dog.java
+++ src/main/java/pl/coderslab/oop/firstclass/Dog.java
@@ -1,4 +1,7 @@
 package pl.coderslab.oop.firstclass;
 
 public class Dog {
+    public String toString() {
+        return "My string representation of this instance of class Dog";
+    }
 }
--- src/main/java/pl/coderslab/oop/firstclass/Main01.java
+++ src/main/java/pl/coderslab/oop/firstclass/Main01.java
@@ -1,8 +1,11 @@
 package pl.coderslab.oop.firstclass;
 
 public class Main01 {
-
+    public static String staticField;
+    public String nonStaticField;
     public static void main(String[] args) {
+        Dog dog = new Dog();
+        System.out.println(dog);
 
     }
 }
