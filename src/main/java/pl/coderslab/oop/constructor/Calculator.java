40ca884eda5291f28354e6d3c39ca141dd58b04e
Rafal <rafal.opial@coderslab.pl>
2020-12-13 12:41:18 +0100

    m2d2t1z1 przygotowanie - dodanie klasy Calculator

---
+++ src/main/java/pl/coderslab/oop/constructor/Calculator.java
@@ -0,0 +1,28 @@
+package pl.coderslab.oop.constructor;
+
+import java.util.Arrays;
+
+public class Calculator {
+    private String[] history;
+
+    public Calculator(){
+        this.history = new String[0];
+    }
+
+    private void addToHistory(String entry){
+        this.history = Arrays.copyOf(this.history, this.history.length+1);
+        this.history[this.history.length-1] = entry;
+    }
+
+    public void showHistory(){
+        for (String entry : this.history){
+            System.out.println(entry);
+        }
+    }
+
+    public int add(int a, int b){
+        int result = a+b;
+        addToHistory("added "+a+" to "+b+" given "+result);
+        return result;
+    }
+}
