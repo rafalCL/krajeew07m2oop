50f2beaf1ec11b76d3670e972ff973c7ab61b06f
Rafal <rafal.opial@coderslab.pl>
2020-12-13 16:09:11 +0100

    m2d2t2z1 static methods and fields

--- src/main/java/pl/coderslab/oop/inheritance/AdvancedCalculator.java
+++ src/main/java/pl/coderslab/oop/inheritance/AdvancedCalculator.java
@@ -2,7 +2,13 @@ package pl.coderslab.oop.inheritance;
 
 import pl.coderslab.oop.constructor.Calculator;
 
+import java.util.Arrays;
+
 public class AdvancedCalculator extends Calculator {
+    //    1. stałą ```PI```, która będzie miała przypisaną wartość **3.14159265**,
+    private static final double PI = 3.14159265;
+    // 3. statyczną tablicę, która będzie przechowywała historię operacji wykonanych na wszystkich kalkulatorach,
+    private static String[] staticHistory = new String[0];
 //    1. ```pow(num1, num2)``` &ndash; metoda ma zwracać ```num1``` do potęgi ```num2```.
 //    Dodatkowo w tablicy operacji ma zapamiętać napis: "```num1```^```num2``` equals ```result```".
     public double pow(double num1, double num2) {
@@ -15,7 +21,28 @@ public class AdvancedCalculator extends Calculator {
 //    Dodatkowo w tablicy operacji ma zapamiętać napis: "```num2``` root of ```num1``` equals ```result```".
     public double root(double num1, double num2) {
         double result = Math.pow(num1, 1/num2);
-        addToHistory(num2 + " root of " + num1 + " equals "+result);
+        String entry = num2 + " root of " + num1 + " equals "+result;
+        addToHistory(entry);
+        addStaticOperations(entry);
         return result;
+    }
+//
+
+//            2. statyczną metodę ```computeCircleArea(r)``` , która będzie zwracała pole koła. Ta metoda nie będzie dopisywać obliczeń do tablicy (napisz w komentarzu, dlaczego nie może tego robić),
+    public static double computeCircleArea(double r){
+        return PI*r*r;
+        // history is nonstatic so we cannot refer to it in a static method computeCircleArea
+    }
+
+    public static void addStaticOperations(String newEntry){
+        staticHistory = Arrays.copyOf(staticHistory, staticHistory.length+1);
+        staticHistory[staticHistory.length-1] = newEntry;
+    }
+
+    // 4. statyczną metodę `printGlobalOperations()`, która będzie wyświetlała wszystkie operacje ze wszystkich obiektów klasy `Calculator`.
+    public static void printGlobalOperations(){
+        for(String entry : staticHistory){
+            System.out.println(entry);
+        }
     }
 }
--- src/main/java/pl/coderslab/oop/inheritance/AdvancedCalculatorTest.java
+++ src/main/java/pl/coderslab/oop/inheritance/AdvancedCalculatorTest.java
@@ -2,11 +2,34 @@ package pl.coderslab.oop.inheritance;
 
 public class AdvancedCalculatorTest {
     public static void main(String[] args) {
+        example2();
+    }
+
+    private static void example1(){
         AdvancedCalculator ac = new AdvancedCalculator();
         System.out.println(ac.add(2,3));
         System.out.println(ac.pow(2,4));
         System.out.println(ac.root(9,2));
         System.out.println(ac.root(8,3));
         ac.showHistory();
+
+
+        System.out.println(AdvancedCalculator.computeCircleArea(3));
+    }
+
+    private static void example2(){
+        AdvancedCalculator ac1 = new AdvancedCalculator();
+        System.out.println(ac1.root(8,3));
+
+        AdvancedCalculator ac2 = new AdvancedCalculator();
+        System.out.println(ac2.root(9,2));
+
+
+        ac1.showHistory();
+        System.out.println("-----------------------");
+        ac2.showHistory();
+
+        System.out.println("-----------------------");
+        AdvancedCalculator.printGlobalOperations();
     }
 }
